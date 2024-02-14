86f3f03dfaeca45748fb6af180c4bb630d0a20ef
santiagor06 <santiago.restrepo06@hotmail.com>
2024-02-14 15:02:23 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'todoapp';
+  nombre = 'Santiago';
+  tasks = ['Comer', 'Correr', 'Estudiar', 'Dormir'];
 }
