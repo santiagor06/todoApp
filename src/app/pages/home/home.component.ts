6d545f89a6cbe7ed2e360ce61794b7b8b621d352
santiagor06 <santiago.restrepo06@hotmail.com>
2024-02-15 11:34:52 -0500

    edit mode

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,15 +2,24 @@ import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ITask } from '../../models/Taks.model';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, ReactiveFormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
 export class HomeComponent {
+  inputTaskControl = new FormControl('', {
+    nonNullable: true,
+    validators: [Validators.required],
+  });
+  editTaskControl = new FormControl('', {
+    nonNullable: true,
+    validators: [Validators.required],
+  });
   tasks = signal<ITask[]>([
     {
       id: Date.now(),
@@ -25,14 +34,58 @@ export class HomeComponent {
       completed: false,
     };
   }
-  addTask(event: Event) {
+  addTask() {
+    if (this.inputTaskControl.valid) {
+      let value = this.inputTaskControl.value.trim();
+      if (value) {
+        let newTask = this.createTask(value);
+        this.tasks.update((tasks) => [...tasks, newTask]);
+        this.inputTaskControl.setValue('');
+      }
+    }
+  }
+  editTask(id: number, event: Event) {
     let input = event.target as HTMLInputElement;
-    let { value } = input;
-    let newTask = this.createTask(value);
-    this.tasks.update((tasks) => [...tasks, newTask]);
-    input.value = '';
+    let value = input.value.trim();
+    if (value) {
+      this.tasks.update((tasks) =>
+        tasks.map((task) => {
+          if (task.id == id) {
+            return {
+              ...task,
+              title: value,
+              editing: false,
+            };
+          }
+          return task;
+        })
+      );
+    }
+  }
+  handleEdit(id: number) {
+    this.tasks.update((tasks) =>
+      tasks.map((task) => {
+        if (task.id == id) {
+          return {
+            ...task,
+            editing: true,
+          };
+        }
+        return { ...task, editing: false };
+      })
+    );
   }
   deleteTak(id: number) {
     this.tasks.update((tasks) => tasks.filter((task) => task.id != id));
+  }
+  updateTask(id: number) {
+    this.tasks.update((tasks) =>
+      tasks.map((task) => {
+        if (task.id == id) {
+          return { ...task, completed: !task.completed };
+        }
+        return task;
+      })
+    );
   }
 }
--- src/app/pages/lab/lab.component.ts
+++ src/app/pages/lab/lab.component.ts
@@ -1,23 +1,34 @@
 import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-lab',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ReactiveFormsModule],
   templateUrl: './lab.component.html',
   styleUrl: './lab.component.css',
 })
 export class LabComponent {
+  constructor() {
+    this.controlColor.valueChanges.subscribe((value) => console.log(value));
+  }
+  widthControl = new FormControl();
+  controlColor = new FormControl();
+  controlName = new FormControl('', {
+    nonNullable: true,
+    validators: [Validators.minLength(4), Validators.required],
+  });
+
   nombre = signal('Santiago');
   tasks = signal(['Comer', 'Correr', 'Estudiar', 'Dormir']);
-  user = {
+  user = signal({
     name: 'Santiago',
     age: 23,
     avatar:
       'https://w7.pngwing.com/pngs/340/946/png-transparent-avatar-user-computer-icons-software-developer-avatar-child-face-heroes-thumbnail.png',
-  };
+  });
   handeClick() {
     alert('Diste Click');
   }
@@ -29,5 +40,15 @@ export class LabComponent {
     let input = event.target as HTMLInputElement;
     let { value } = input;
     this.nombre.set(value);
+  }
+  changeName(event: Event) {
+    let input = event.target as HTMLInputElement;
+    let { value } = input;
+    this.user.update((user) => {
+      return {
+        ...user,
+        name: value,
+      };
+    });
   }
 }
